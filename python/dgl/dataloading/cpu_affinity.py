fca5a736dfa9f6f38ca36263af7015b2c3dcb44f
Pawel Piotrowicz <pawel.piotrowicz@intel.com>
2021-08-11 21:49:59 +0200

    Sasi Changes affinity

---
+++ python/dgl/dataloading/cpu_affinity.py
@@ -0,0 +1,12 @@
+from .. import backend as F
+from .. import ndarray
+from .. import utils
+from .._ffi.function import _init_api
+
+
+def PinOMPThreads(x):
+    x = F.to_dgl_nd(F.tensor(x, dtype=F.int64))
+    _CAPI_SetOMPThreadAffinity(x)
+
+
+_init_api("dgl.dataloading.cpu_affinity")
